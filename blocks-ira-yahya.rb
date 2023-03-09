68794f4a012befc63b6919b216a7fc8507d0d39a
iraholmes <ira.holmes63@gmail.com>
2023-03-09 14:09:00 -0800

    Ruby blocks challenge updated with some answers

---
+++ blocks-ira-yahya.rb
@@ -0,0 +1,65 @@
+# Create a loop that prints the numbers 1 to 20.
+# first_range = 1
+# while first_range <= 20
+#     p first_range
+#     first_range +=1
+# end
+
+# Create a loop that prints the numbers 1 to 20 using a different approach than previously used.
+# num = 1..20
+# num.each do |value|
+#     p value
+# end
+
+# Create a loop that prints only even numbers from 20 to 0.
+# num_loop = 0..20
+# num_loop.each. do |value|
+# if value.even?
+#     p value
+#     end
+# end
+
+# Create a method that takes in an array of numbers and returns a new array with all the numbers multiplied by 5.
+# array = [2, 4, 6, 8]
+# mapped = array.map do |value|
+#     value *5
+# end
+# p mapped
+# Create a method that takes in an array of lowercase words and capitalizes all of the words, permanently modifying the original array.
+# word_array = ["yes", "no", "maybe", "so"]
+# mapped = word_array.map do |value|
+#     value.capitalize!
+# end
+# p mapped
+# Create a method that takes in an array of numbers and returns the largest number in the array.
+array_one = [20, 25, 50, 75, 70, 55]
+# def largest_num array 
+#     #     array.select do |value|
+#     #         value.max()
+#     # end  
+#     array.max
+# end
+# p largest_num(array_one)
+
+# Create a method that takes in an array of words and returns the word with the least number of characters.
+# words = ["ooga", "boogaa", "boo" ]
+# def least_char array
+#     array.min()
+# end
+# p least_char(words)
+# Create a method that takes in an array of numbers and returns only the odd numbers sorted from least to greatest.
+def sorted array
+    array.select do |value|
+    array_one.even?
+    
+    end
+end   
+p array_one.sort
+
+# Create a method that takes in an array of strings and returns all the strings in reverse casing. All uppercase letters should be returned lowercased and all lowercased letters should be returned in uppercase letters.
+
+
+# Create a method that will take in an array of numbers and return an array with the remainder of each number when divided by 2.
+
+
+# Create a method that will take in a range of letters and return an array with all the letters capitalized.
