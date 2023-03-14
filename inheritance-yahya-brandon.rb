e6aa8f0295750c3716952264c1f0fd4a2b15b396
yiahmed <yahyahmed28@gmail.com>
2023-03-14 12:03:01 -0700

    added some answers to challenges

---
+++ inheritance-yahya-brandon.rb
@@ -0,0 +1,73 @@
+# 🐟 Challenges: Animal Kingdom
+# Read all stories before starting the challenge.
+# As a developer, I can make a generic Animal class.
+# As a developer, upon initialization, I can give my Animal a status of alive, which will be set to true.
+# As a developer, I can give my Animal an age of 0 upon initialization.
+# As a developer, I can age my Animal up one year at a time.
+# As a developer, I can return my Animal's age, as well as if they're alive.
+# p dog = Animal.new('Dog')
+# p dog #<Animal:0x000000010d3c1460 @name="Dog", @alive=true, @age=0>
+# Hint: Use attr_accessor as well as an initialize method.
+# As a developer, I can create a Fish that inherits from Animal.
+# As a developer, I can initialize all of my fish to be cold_blooded. (Yes, there is one fish who is technically fully warm-blooded but we aren't going to talk about that.)
+# As a developer, I can create a Salmon that inherits from Fish.
+# As a developer, I can initialize my Salmon to be a specific species (Atlantic, Sockeye, etc).
+# As a developer, I can see that my Salmon is cold-blooded.
+class Animal 
+    attr_accessor :name, :alive, :age
+    def initialize (age)
+        @name = name
+        @alive = true
+        @age = age
+    end
+    def age_up 
+        @age =+ 1
+    end
+end
+
+class Fish < Animal
+    def initialize
+        super(name_parameter)
+        @cold_blood = true
+    end
+end
+class Salmon < Fish
+    def initialize (species, name_parameter)
+        super(@cold_blood, name_parameter)
+        @species = species
+    end
+    def get_fish_data
+        if @cold_blood
+        "This #{@species} of salmon is cold-blooded."
+        else
+        "This #{@species} of salmon is not cold-blooded."
+        end
+    end
+end
+salmon = Salmon.new('Atlantic', 0)
+salmon.age_up
+p salmon
+
+# As a developer, I can age my Salmon up.
+# As a developer, I can see a message that tells me all of my Salmon's information.
+# As a developer, if my Salmon reaches the ripe old age of 4, I can make it die peacefully after a full and happy life.
+# Hint: You will need a method that changes the status of alive in the initialize method of Animal.
+# As a developer, I can create a Mammal that inherits from Animal.
+# As a developer, I can initialize all of my Mammals to be warm_blooded.
+# As a developer, I can create a Bear that inherits from Mammal.
+# As a developer, I can age my Bear up.
+# As a developer, I can see a message that tells me all of my Bear's information.
+# As a developer, if my Bear turns 20 years old, I can make it die peacefully after a full and happy life.
+# Hint: You will need a method that changes the status of alive in the initialize method of Animal.
+# As a developer, I can create a Mammal of my choice.
+# As a developer, I can interact with the new Mammal via various methods.
+# As a developer, I can see a message that tells me all of my new Mammal's information.
+
+
+# 🏔 Stretch Goals
+# As a developer, I can keep a collection of two of each Animal.
+# Hint: You'll want to add your Animals into an array.
+# As a developer, I can sort my collection of Animals based on age.
+# Hint: Find out how the spaceship operator can help you with an array.
+# As a developer, I can utilize a Ruby module to help DRY up my code. I can create a swim method inside of my module that will apply to Animals who can swim. This method should return "I can swim!"
+# Hint: Look into module mix ins. Since not all animals can swim, only certain Animals will have access to this module.
